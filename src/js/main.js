fc266574db24e6184607bda9cb3cb22c7e9d1464
Roman Kuchin <roman.kuchin@mail.com>
2024-01-07 14:34:59 +0500

    add slider swiper

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,12 @@
-// import './_vendor';
-// import vars from './_vars';
-// import './_functions';
-// import './_components';
-import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import Swiper from "swiper";
+import { Navigation } from "swiper";
 
-// init Swiper:
-// const swiper = new Swiper('.swiper', {
-//   // configure Swiper to use modules
-//   // modules: [Navigation, Pagination],
+const swiper = new Swiper(".slider", {
+  lazy: true,
+  modules: [Navigation],
 
-// });
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  // loop: true,
-  rewind: true,
-
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".slider__btn--next",
+    prevEl: ".slider__btn--prev",
   },
-
 });
