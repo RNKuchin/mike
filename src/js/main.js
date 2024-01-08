b97fb17826fae3d401152ad46f51d7a87464530e
Roman Kuchin <roman.kuchin@mail.com>
2024-01-08 23:11:31 +0500

    add mobile menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,32 @@
+// import "./_functions";
 import Swiper from "swiper";
 import { Navigation } from "swiper";
+
+const navMenu = (
+  burgerSelector = ".burger",
+  navSelector = ".nav",
+  classOpen = "open"
+) => {
+  const page = document.querySelector("html");
+  const burger = page.querySelector(burgerSelector);
+  const nav = page.querySelector(navSelector);
+
+  const toggleFunc = () => {
+    burger.classList.toggle(classOpen);
+    nav.classList.toggle(classOpen);
+    page.style.cssText = burger.classList.contains("open")
+      ? "overflow:hidden;"
+      : "overflow:auto;";
+  };
+
+  burger.addEventListener("click", () => {
+    toggleFunc();
+  });
+  nav.addEventListener("click", () => {
+    toggleFunc();
+  });
+};
+navMenu();
 
 const swiper = new Swiper(".slider", {
   lazy: true,
